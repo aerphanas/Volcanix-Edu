f6bffcb77dee8507d7a6bbb61055815792cabcf9
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-01-28 08:32:11 +0700

    add sort & binary search from Arrays object
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- Jan/26-jan/Main.java
+++ Jan/26-jan/Main.java
@@ -1,16 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
 
   public static void main(String[] args) {
     int[] arr = {9,8,7,6,4,5,3,2,1};
-
+    int[] arrs = {9,8,7,6,4,5,3,2,1};
 
     //find number 7
     for (int i = 0; i < arr.length; i++) {
       if (arr[i] == 7)
-        System.out.printf("Number 7 found in arr[%d]", i);
+        System.out.printf("Number 7 found in arr[%d]\n", i);
     }
-
-    System.out.println("");
 
     // print member of arr
     for (int i : arr)
@@ -27,6 +27,21 @@ public class Main {
       System.out.print(arr[i] + " ");
     }
     System.out.println("");
+
+    System.out.println("======================");
+
+    for (int i : arrs)
+      System.out.print(i + " ");
+
+    System.out.println("");
+    Arrays.sort(arrs);
+
+    for (int i : arrs)
+      System.out.print(i + " ");
+
+      System.out.println("");
+
+      System.out.printf("Number 7 found in arr[%s]\n",Arrays.binarySearch(arrs, 7));
   }
 
   public static void swapMember(int[] arr, int i, int j) {
