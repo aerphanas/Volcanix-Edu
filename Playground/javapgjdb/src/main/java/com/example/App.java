ceedba6127c245a0f88b7c437cb1b89b21b336aa
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-01-30 16:51:49 +0700

    test database postgresql
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- Playground/javapgjdb/src/main/java/com/example/App.java
+++ Playground/javapgjdb/src/main/java/com/example/App.java
@@ -1,13 +1,26 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String host = "";
+        String password = "";
+        String user = "";
+        String database = "";
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://"+ host +":5432/" + database, user, password);
+            System.out.println("Opened database successfully");
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("database closed successfully");
     }
 }
