6ae054608e30ec35a91133b6f826d4bba73156b2
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-02-16 17:43:14 +0700

    cleaning code
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- Week-04/Day-04/quarkus-getting-started/src/test/java/org/acme/GreetingResourceTest.java
+++ Week-04/Day-04/quarkus-getting-started/src/test/java/org/acme/GreetingResourceTest.java
@@ -3,19 +3,32 @@ package org.acme;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Test
+    @Test    
     public void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .statusCode(200)    
+             .body(is("Hello World"));
+    }
+
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+          .pathParam("name", uuid)
+          .when().get("/hello/greeting/{name}")
+          .then()
+            .statusCode(200)
+            .body(is("Hello " + uuid));
     }
 
 }
