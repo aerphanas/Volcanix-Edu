8200f43c1b481218c89222f894f16f321778cc39
Λdiƛin <muhamadaviv14@gmail.com>
2023-02-19 20:25:17 +0700

    fix typo

--- assignment/week-4/TugasOne.java
+++ assignment/week-4/TugasOne.java
@@ -3,12 +3,12 @@
  *                                                                        *
  *   Copyright (C) 2023 aerphanas                                         *
  *                                                                        *
- *   Crypted is free software: you can redistribute it and/or modify      *
+ *   this is free software: you can redistribute it and/or modify         *
  *   it under the terms of the GNU General Public License as published    *
  *   by the Free Software Foundation, either version 3 of the License,    *
  *   or (at your option) any later version.                               *
  *                                                                        *
- *   Crypted is distributed in the hope that it will be useful,           *
+ *   this is distributed in the hope that it will be useful,              *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty          *
  *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.              *
  *   See the GNU General Public License for more details.                 *
