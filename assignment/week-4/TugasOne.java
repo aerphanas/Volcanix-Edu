719b03412815d6a2b62485416d515458ebae7a04
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-02-20 06:53:56 +0700

    fix dddd, mmmm yy date format, delete gpl license and fix stdout
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- assignment/week-4/TugasOne.java
+++ assignment/week-4/TugasOne.java
@@ -1,24 +1,3 @@
-/**************************************************************************
- *   TugasOne.java                                                        *
- *                                                                        *
- *   Copyright (C) 2023 aerphanas                                         *
- *                                                                        *
- *   this is free software: you can redistribute it and/or modify         *
- *   it under the terms of the GNU General Public License as published    *
- *   by the Free Software Foundation, either version 3 of the License,    *
- *   or (at your option) any later version.                               *
- *                                                                        *
- *   this is distributed in the hope that it will be useful,              *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty          *
- *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.              *
- *   See the GNU General Public License for more details.                 *
- *                                                                        *
- *   You should have received a copy of the GNU General Public License    *
- *   along with this program.  If not, see http://www.gnu.org/licenses/.  *
- *                                                                        *
- **************************************************************************/
-
-
 /* 1. Regular Expression
  *    Buatlah method yang untuk mengecek pattern pada string apakah bertipe email,
  *    pattern Tanggal (dd-MM-yyyy,)  Pattern Tanggal  (dddd, MMMM yy) atau IP Address.
@@ -32,7 +11,7 @@ public class TugasOne {
         // melakukan pengecekan jika fungsi bisa berjalan
         whatIsThis("muhammadaviv14@gmail.com");
         whatIsThis("19-01-2038");
-        whatIsThis("19, 01 2038");
+        whatIsThis("Jumat, January 38");
         whatIsThis("127.0.0.1");
 
         // mengecek juga bila input bukan termasuk
@@ -47,8 +26,8 @@ public class TugasOne {
         // dalam string
         final String pattern[] = {
             "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", // email
-            "^(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-\\d{4}$", // date dd-MM-yyyy
-            "^(0[1-9]|[1-2][0-9]|3[0-1]), (0[1-9]|1[0-2]) \\d{4}$", // date dd, MM yyyy
+            "^(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-\\d{4}$", // date DD-MM-YYYY
+            "^[A-Z][a-z]{3,8}, [A-Z][a-z]{2,8} \\d{2}$", // date dddd, MMMM YY
             "\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b" // Ip addr
         };
 
@@ -58,7 +37,7 @@ public class TugasOne {
         Matcher matcher = null;
 
         // untuk mengecek setiap regex
-        for (int i = 0; i < pattern.length;i++) {
+        for (int i = 0;i < pattern.length;i++) {
             switch (i) {
                 // pattern pertama dalah email
                 case 0:
@@ -75,7 +54,7 @@ public class TugasOne {
                     regex = Pattern.compile(pattern[i]);
                     matcher = regex.matcher(input);
                     if(matcher.find()){
-                        System.out.println("Pattern Matched : This is Date dd-mm-yyyy");
+                        System.out.println("Pattern Matched : This is Date DD-MM-YYYY");
                         return true;
                     }
                     break;
@@ -85,7 +64,7 @@ public class TugasOne {
                     regex = Pattern.compile(pattern[i]);
                     matcher = regex.matcher(input);
                     if(matcher.find()){
-                        System.out.println("Pattern Matched : This is Date dd, mm yyyy");
+                        System.out.println("Pattern Matched : This is Date dddd, MMMM YY");
                         return true;
                     }
                     break;
