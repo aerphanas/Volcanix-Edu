7314116d5508c15e6428101074ac7e5e4e56527d
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-02-28 11:33:38 +0700

    update api
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- Playground/quarkus-api/src/main/java/org/acme/Rest.java
+++ Playground/quarkus-api/src/main/java/org/acme/Rest.java
@@ -19,7 +19,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/api")
-@Produces(MediaType.APPLICATION_JSON)
+@Produces("application/json")
 @Consumes(MediaType.APPLICATION_JSON)
 public class Rest {
 
