c205d933cd7fb916f7ee880059ce59cf4363db2d
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-03-03 20:43:25 +0700

    add database entity
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

---
+++ assignment/week-6/crud/src/main/java/org/acme/Package.java
@@ -0,0 +1,98 @@
+package org.acme;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+
+
+@Entity
+@Table(name = "package", schema="packages")
+public class Package extends PanacheEntityBase {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id")
+    public String id;
+
+    @Column(name = "Name")
+    public String name;
+    
+    @Column(name = "Architecture")
+    public String arch;
+
+    @Column(name = "Description")
+    public String repo;
+    
+    @Column(name = "URL")
+    public String url;
+
+    @Column(name = "Maintainer")
+    public String maintainer;
+
+    @Column(name = "License")
+    public String license;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArch() {
+        return arch;
+    }
+
+    public void setArch(String arch) {
+        this.arch = arch;
+    }
+
+    public String getRepo() {
+        return repo;
+    }
+
+    public void setRepo(String repo) {
+        this.repo = repo;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getMaintainer() {
+        return maintainer;
+    }
+
+    public void setMaintainer(String maintainer) {
+        this.maintainer = maintainer;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+}
