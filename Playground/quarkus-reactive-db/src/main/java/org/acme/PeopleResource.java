a538261b0ec7737dffdbb94b918d18e10fe3b9b0
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-03-03 08:48:47 +0700

    add PUT endpoint for reactive database
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- Playground/quarkus-reactive-db/src/main/java/org/acme/PeopleResource.java
+++ Playground/quarkus-reactive-db/src/main/java/org/acme/PeopleResource.java
@@ -5,6 +5,7 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -15,7 +16,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Uni;
 
 @Path("/db")
@@ -24,47 +25,64 @@ public class PeopleResource {
 
         @GET
         public Uni<Response> get() {
+            Log.info("Request GET Method");
             return People.listAll()
                     .onItem().transform(row -> Response.ok(row).build())
                     .onFailure().recoverWithItem(Response.serverError().build());
         }
 
         @POST
-        public Uni<Response> addPeople(String requestBody) {
-            JsonObject json = Json.createReader(new StringReader(requestBody)).readObject();
+        public Uni<Response> addPeople(JsonObject requestBody) {
     
+            Log.info("Request POST Method with " + requestBody);
             People person = new People();
-            person.setName(json.getString("name"));
-            person.setUmur(json.getInt("age"));
-            person.setId(json.getInt("id"));
+            person.setName(requestBody.getString("name"));
+            person.setUmur(requestBody.getInt("age"));
+            person.setId(requestBody.getInt("id"));
     
             return person.persistAndFlush()
                     .onItem().transform(rows -> Response.ok("ok").build())
                     .onFailure().recoverWithItem(Response.serverError().build());
         }
 
-        // TODO fix this shit
         @PUT
         @Path("{id}")
-        public Uni<Response> updatePeople(@PathParam("id") Integer id, String requestBody) {
-            JsonObject json = Json.createReader(new StringReader(requestBody)).readObject();
-            People person = new People();
-            return People.findById(id)
-                    .onItem().ifNull().failWith(() -> new WebApplicationException("Record not found", 404))
-                    .onItem().ifNotNull().transform(entity -> {
-                        person.setName(json.getString("name"));
-                        person.setUmur(json.getInt("age"));
-                        return entity;
+        public Uni<Response> putPeople(@PathParam("id") Integer id, JsonObject requestBody) {
+            Log.info("Request PUT Method with id " + id + " and " + requestBody);
+            return People.<People>findById(id)
+                    .onItem().ifNull().failWith(new NotFoundException())
+                    .onItem().transform(person -> {
+                        person.setName(requestBody.getString("name"));
+                        person.setUmur(requestBody.getInt("age"));
+                        return person;
                     })
-                    .onItem().ifNotNull().transform(PanacheEntityBase::persistAndFlush)
-                    .onItem().transform(rows -> rows == null ? 0 : rows)
-                    .onItem().transform(rows -> rows != null ? Response.ok("ok").build() : Response.status(Status.NOT_MODIFIED).build())
+                    .onItem().transformToUni(person -> person.persistAndFlush())
+                    .onItem().transform(x -> Response.ok("ok").build())
                     .onFailure().recoverWithItem(Response.serverError().build());
         }
+        
+
+        // TODO fix this shit
+        // @PUT
+        // @Path("{id}")
+        // public Uni<Response> updatePeople(@PathParam("id") Integer id, JsonObject requestBody) {
+        //     return People.<People>findById(id)
+        //             .onItem().ifNotNull().transform(person -> {
+        //                 person.setName(requestBody.getString("name"));
+        //                 person.setUmur(requestBody.getInt("age"));
+        //                 return person;
+        //             })
+        //             .onItem().ifNull().failWith(new WebApplicationException("Person with id " + id + " not found", Response.Status.NOT_FOUND))
+        //             .flatMap(person -> person.persistAndFlush())
+        //             .onItem().transform(rows -> {
+        //                 return rows != null ? Response.ok(rows).build() : Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        //             });
+        // }
 
         @DELETE
         @Path("{id}")
         public Uni<Response> deletePeople(Integer id) {
+            Log.info("Request DELETE Method to id " + id);
             return People.delete("id", id)
                     .onItem().transform(rows -> {
                         return rows > 0 ? Response.noContent().build()
