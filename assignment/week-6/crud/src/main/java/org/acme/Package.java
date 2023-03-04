a90a9348fc53205618bd7acc66fd40205bcc7ce9
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-03-04 19:29:31 +0700

    add try catch for safety
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- assignment/week-6/crud/src/main/java/org/acme/Package.java
+++ assignment/week-6/crud/src/main/java/org/acme/Package.java
@@ -10,7 +10,6 @@ import org.hibernate.annotations.GenericGenerator;
 
 import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
 
-
 @Entity
 @Table(name = "package", schema="packages")
 public class Package extends PanacheEntityBase {
@@ -19,27 +18,29 @@ public class Package extends PanacheEntityBase {
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(name = "id")
-    public String id;
+    private String id;
 
     @Column(name = "Name")
-    public String name;
+    private String name;
     
     @Column(name = "Architecture")
-    public String arch;
+    private String arch;
 
     @Column(name = "Description")
-    public String desc;
+    private String desc;
     
     @Column(name = "URL")
-    public String url;
+    private String url;
 
     @Column(name = "Maintainer")
-    public String maintainer;
+    private String maintainer;
 
     @Column(name = "License")
-    public String license;
+    private String license;
+    
+    public Package() {
+    }
 
-    // Getter & Setter
     public String getId() {
         return id;
     }
@@ -94,6 +95,59 @@ public class Package extends PanacheEntityBase {
 
     public void setLicense(String license) {
         this.license = license;
+    }
+
+    // Untuk Builder Pattern
+    private Package(PackageBuilder builder) {
+        this.name = builder.name;
+        this.arch = builder.arch;
+        this.desc = builder.desc;
+        this.url = builder.url;
+        this.maintainer = builder.maintainer;
+        this.license = builder.license;
+	}
+
+    public static class PackageBuilder {
+        private String name;
+        private String arch;
+        private String desc;
+        private String url;
+        private String maintainer;
+        private String license;
+
+        public PackageBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PackageBuilder setArch(String arch) {
+            this.arch = arch;
+            return this;
+        }
+
+        public PackageBuilder setDesc(String desc) {
+            this.desc = desc;
+            return this;
+        }
+
+        public PackageBuilder setUrl(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public PackageBuilder setMaintainer(String maintainer) {
+            this.maintainer = maintainer;
+            return this;
+        }
+
+        public PackageBuilder setLicense(String license) {
+            this.license = license;
+            return this;
+        }
+
+        public Package build() {
+			return new Package(this);
+		}
     }
 
 }
--- assignment/week-6/crud/src/main/java/org/acme/PackageResource.java
+++ assignment/week-6/crud/src/main/java/org/acme/PackageResource.java
@@ -6,16 +6,15 @@ import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
 import javax.json.JsonObject;
 
 import org.jboss.logging.Logger;
 
-import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
 import io.smallrye.mutiny.Uni;
 
 @Path("/packages")
@@ -40,32 +39,66 @@ public class PackageResource {
     public Uni<Response> postPackage(JsonObject requestBody) {
         LOG.info("Request POST " + requestBody + " to database");
 
-        Package pkg = new Package();
+        String name, arch, desc, url, maintainer, license;
 
+        // try catch agar bila terjadi error pada JsonObject
+        // maka bisa ditangani dengan mengirim bad request ke client
+        try {
+
+            name = requestBody.getString("Name");
+            arch = requestBody.getString("Architecture");
+            desc = requestBody.getString("Description");
+            url = requestBody.getString("URL");
+            maintainer = requestBody.getString("Maintainer");
+            license = requestBody.getString("License");
+
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+            LOG.error("Maybe some object in Request Body is null ?");
+            return Uni.createFrom().item(Response.status(Response.Status.BAD_REQUEST).build());
+        }
+
+        Package pkgsin = new Package.PackageBuilder()
+                                    .setName(name)
+                                    .setArch(arch)
+                                    .setDesc(desc)
+                                    .setUrl(url)
+                                    .setMaintainer(maintainer)
+                                    .setLicense(license)
+                                    .build();
+    
         // menggunakan Package Find akan mengetahui bila nama yang akan dimasuki
         // ke database sudah ada atau belum, bila sudah ada maka membuat exception yang
         // nanti akan ditangkap oleh onFailure bila tidak maka akan lanjut menambahkan
         // request ke database
-        return Package.find("Name", requestBody.getString("Name")).firstResult()
-            .onItem().ifNotNull().failWith(() -> new NotFoundException("Package not found in database"))
-            .onItem().transformToUni( x -> {
-                pkg.setName(requestBody.getString("Name"));
-                pkg.setArch(requestBody.getString("Architecture"));
-                pkg.setDesc(requestBody.getString("Description"));
-                pkg.setUrl(requestBody.getString("URL"));
-                pkg.setMaintainer(requestBody.getString("Maintainer"));
-                pkg.setLicense(requestBody.getString("License"));
-                return pkg.persistAndFlush();
-            })
-            .onItem().transform(rows -> Response.ok("ok").build())
-            .onFailure().recoverWithItem(Response.serverError().build());
+        return Package.find("Name", name).firstResult()
+                .onItem().ifNotNull().failWith(() -> new NotFoundException("Package not found in database"))
+                .onItem().transformToUni(x -> pkgsin.persistAndFlush())
+                .onItem().transform(rows -> Response.ok("ok").build())
+                .onFailure().recoverWithItem(Response.serverError().build());
     }
-
+    
 
     @PUT
     @Path("{name}")
     public Uni<Response> putPackage(String name, JsonObject requestBody) {
         LOG.info("Request PUT " + requestBody + " to database");
+
+        String arch, desc, url, maintainer, license;
+        
+        try {
+
+            arch = requestBody.getString("Architecture");
+            desc = requestBody.getString("Description");
+            url = requestBody.getString("URL");
+            maintainer = requestBody.getString("Maintainer");
+            license = requestBody.getString("License");
+
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+            LOG.error("Maybe some object in Request Body is null ?");
+            return Uni.createFrom().item(Response.status(Response.Status.BAD_REQUEST).build());
+        }
 
         // hampir sama dengan method POST tetapi bedanya method ini mengecek
         // apakah nama tidak ada dalam database, bila tidak ada kita tidak dapat
@@ -73,11 +106,9 @@ public class PackageResource {
         return Package.find("Name", requestBody.getString("Name")).firstResult()
             .onItem().ifNull().failWith(() -> new NotFoundException("Package not found in database"))
             .onItem().transformToUni( x -> {
-                return Package.update("Name = ?1, Architecture = ?2, Description = ?3, URL = ?4, Maintainer = ?5, License = ?6 where Name = ?7"
-                        , requestBody.getString("Name"), requestBody.getString("Architecture")
-                        , requestBody.getString("Description"), requestBody.getString("URL")
-                        , requestBody.getString("Maintainer"), requestBody.getString("License")
-                        , requestBody.getString("Name"));
+                return Package.update(
+                    "Name = ?1, Architecture = ?2, Description = ?3, URL = ?4, Maintainer = ?5, License = ?6 where Name = ?7"
+                    , name, arch, desc, url, maintainer, license, name);
             })
             .onItem().transform(rows -> Response.ok("ok").build())
             .onFailure().recoverWithItem(Response.status(Status.BAD_REQUEST).build());
