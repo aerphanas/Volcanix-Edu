87c028a71c4f11287926b50d013118b5024ffd70
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-03-09 18:08:24 +0700

    add Uni method
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- Week-06/Day-01/poi-excel/src/main/java/org/acme/GreetingResource.java
+++ Week-06/Day-01/poi-excel/src/main/java/org/acme/GreetingResource.java
@@ -7,12 +7,16 @@ import java.io.IOException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import io.quarkus.logging.Log;
+import io.smallrye.mutiny.Uni;
 
 @Path("/poi")
 public class GreetingResource {
@@ -25,7 +29,7 @@ public class GreetingResource {
         Workbook workbook = new XSSFWorkbook(file);
 
         // Mengambil sheet pertama dari workbook
-        Sheet sheet = workbook.createSheet("shit 2");
+        Sheet sheet = workbook.getSheet("shit 2");
 
         // Mengubah nilai di sel (1,1)
         Row row = sheet.createRow(1);
@@ -40,5 +44,38 @@ public class GreetingResource {
 
         // Menutup file
         file.close();
+
+        Log.info("normal");
+
     }
+
+    @GET
+    @Path("uni")
+    public Uni<Response> unithread() {
+        return Uni.createFrom().item(() -> {
+            try {
+                FileInputStream file = new FileInputStream(new File("test.xlsx"));
+                // Membuat objek workbook dari file
+                Workbook workbook = new XSSFWorkbook(file);
+                // Mengambil sheet pertama dari workbook
+                Sheet sheet = workbook.getSheet("sheet 2");
+                // Mengubah nilai di sel (1,1)
+                Row row = sheet.createRow(1);
+                Cell cell = row.createCell(1);
+                cell.setCellValue("Hello World");
+                // Menyimpan perubahan
+                FileOutputStream fileOut = new FileOutputStream("test.xlsx");
+                workbook.write(fileOut);
+                workbook.close();
+                fileOut.close();
+                file.close();
+                // Menutup file
+                Log.info("uni");
+                return Response.ok().build();
+            } catch (IOException e) {
+                return Response.serverError().build();
+            }
+        });
+    }
+
 }
